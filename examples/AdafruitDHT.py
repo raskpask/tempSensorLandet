31c8f12eebc21c2b990f1469bb588044b767e9c6
jakmol@ug.kth.se <MmR-PZ-Q>
2019-08-20 16:32:16 +0200

    mailing wokring sedning temp and humid

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -35,7 +35,7 @@ def send_mail():
 	msg = Message("Send Mail Tutorial!",
 	    sender="blidohuset@gmail.com",
 		recipients=["molin.jakob@gmail.com"])
-	msg.body = 'Hej!\n Temperaturen i huset är ' #+ get_temp() +' och Luftfuktigheten är ' + get_humid() + '\n MVH Huset'           
+	msg.body = "Hej!\n Temperaturen i huset är " #+ get_temp() +' och Luftfuktigheten är ' + get_humid() + '\n MVH Huset'           
 	mail.send(msg)
 	return 'Mail sent!'
     
