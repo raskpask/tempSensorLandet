17aa47954e0d97a3825aa1fccf23f90ee8537b19
jakmol@ug.kth.se <MmR-PZ-Q>
2019-08-20 15:21:32 +0200

    first try

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -8,11 +8,12 @@ import json
 
 @app.route('/get_temp', methods = ['POST'])
 def get_temp():
-    # sensor_args = { '11': Adafruit_DHT.DHT11,
-    #             '22': Adafruit_DHT.DHT22,
-    #             '2302': Adafruit_DHT.AM2302 }
- 
-    humidity, temperature = Adafruit_DHT.read_retry(2302, 4)
+    sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302 }
+    sensor = sensor_args[2302]
+    pin = 4
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
     else:
