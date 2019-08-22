71652387cea69286acf6fb083950e7104e59d495
jakmol@ug.kth.se <MmR-PZ-Q>
2019-08-22 19:19:27 +0200

    first try with everything

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -1,8 +1,11 @@
 from flask import Flask, redirect, url_for, request, abort
 from flask_mail import Mail, Message
+from mail import Mail_Handler
 import sys
 app = Flask(__name__)
 SERVER_IP = '192.168.1.4'
+MAIL_USERNAME = 'blidohuset@gmail.com'
+MAIL_PASSWORD = 'koppen123'
 import Adafruit_DHT
 import json
 SENSOR = Adafruit_DHT.AM2302
@@ -15,59 +18,38 @@ app.config.update(
 	MAIL_SERVER='smtp.gmail.com',
 	MAIL_PORT=465,
 	MAIL_USE_SSL=True,
-	MAIL_USERNAME = 'blidohuset@gmail.com',
-	MAIL_PASSWORD = 'koppen123'
+	MAIL_USERNAME = MAIL_USERNAME,
+	MAIL_PASSWORD = MAIL_PASSWORD
 	)
 mail = Mail(app)
+mail_handler =Mail_Handler()
 import imaplib
-
-def read(username, password, sender_of_interest):
-    # Login to INBOX
-    imap = imaplib.IMAP4_SSL("imap.gmail.com", 993)
-    imap.login(username, password)
-    imap.select('INBOX')
-
-    # Use search(), not status()
-    status, response = imap.search(None, 'INBOX', '(UNSEEN)')
-    unread_msg_nums = response[0].split()
-
-    # Print the count of all unread messages
-    print(len(unread_msg_nums))
-
-    # Print all unread messages from a certain sender of interest
-    status, response = imap.search(None, '(UNSEEN)', '(FROM "%s")' % (sender_of_interest))
-    unread_msg_nums = response[0].split()
-    da = []
-    for e_id in unread_msg_nums:
-        _, response = imap.fetch(e_id, '(UID BODY[TEXT])')
-        da.append(response[0][1])
-    print(da)
-
-    # Mark them as seen
-    for e_id in unread_msg_nums:
-        imap.store(e_id, '+FLAGS', '\Seen')
-
-
 
 def get_temp():
     humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
     return round(temperature,1) 
+
 def get_humid():
     humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
     return round(humidity,1)
 
+def send_mail(reciver):
+	msg = Message("Temperatur Blido",
+	    sender="blidohuset@gmail.com",
+		recipients=[reciver])
+	msg.body = "Hej!\nTemperaturen i huset: "+ str(get_temp()) + " C"+ chr(176)+" \nLuftfuktighet: "+ str(get_humid()) + "%\nMVH\nHuset"           
+	mail.send(msg)
+	return 'Mail sent!'
+
+def check_new_mails():
+    sender = mail_handler.check_messages(MAIL_USERNAME,MAIL_PASSWORD)
+    if 0 != sender:
+        send_mail(sender)
+        
 @app.route('/get_temp_and_humid', methods = ['POST'])
 def get_temp_and_humid():
     return json.dumps({'temp':get_temp(), 'humidity':get_humid()})
 
-@app.route('/send_mail', methods = ['POST'])
-def send_mail():
-	msg = Message("Temperatur Blido",
-	    sender="blidohuset@gmail.com",
-		recipients=["molin.jakob@gmail.com"])
-	msg.body = "Hej!\nTemperaturen i huset: "+ str(get_temp()) + " C"+ chr(176)+" \nLuftfuktighet: "+ str(get_humid()) + "%\nMVH\nHuset"           
-	mail.send(msg)
-	return 'Mail sent!'
-    
+
 if __name__ == '__main__':
    app.run(host = SERVER_IP, debug=False)
--- examples/mail.py
+++ examples/mail.py
@@ -1,68 +1,44 @@
 import imaplib
 import email
 
-emailaddress = "blidohuset@gmail.com"
-passw = "koppen123"
-imapserver = "imap.gmail.com"
-
-def delete_Unseen_Emails_and_get_user(user, password, IMAP):
-    mail = imaplib.IMAP4_SSL(IMAP)
-    mail.login(user, password)
-    mail.select("inbox")
-    typ, data = mail.search(None, 'ALL')
-    ids = data[0] # data is a list.
-    id_list = ids.split() # ids is a space separated string
-    latest_email_id = id_list[-1] # get the latest
- 
-    result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
-    print('Getting user')
-    raw_email = data[0][1].decode('utf-8') # here's the body, which is raw text of the whole email
-    # including headers and alternate payloads
-    # print(raw_email)
-    email_message = email.message_from_string(raw_email)
-    sender = email.utils.parseaddr(email_message['From'])
-    print(sender)
-    # if isinstance(data, tuple):
-    #     msg = email.message_from_string(data)
-    #     varFrom = msg['from']
-    # varFrom = varFrom.replace('<', '')
-    # varFrom = varFrom.replace('>', '')
-    # if len( varSubject ) > 35:
-    #     varSubject = varSubject[0:32] + '...'
-    # print(varSubject)
-    # for response_part in data:
-    #   if isinstance(response_part, tuple):
-    #       msg = email.message_from_string(response_part[1])s
-    #       varSubject = msg['subject']
-    #       varFrom = msg['from']
-
-    # #remove the brackets around the sender email address
-    # varFrom = varFrom.replace('<', '')
-    # varFrom = varFrom.replace('>', '')
-
-    # #add ellipsis (...) if subject length is greater than 35 characters
-    # if len( varSubject ) > 35:
-    #       varSubject = varSubject[0:32] + '...'
-
-    # print('[' + varFrom.split()[-1] + '] ' + varSubject)
+class Mail_Handler:
     
-    # for num in data[0].split():
-    mail.store(latest_email_id, '+FLAGS', r'(\Deleted)')
-    mail.expunge()
-    mail.close()
-    mail.logout()
-
-def check_messages(emailaddress,passw):
-    mail = imaplib.IMAP4_SSL(imapserver)
-    mail.login(emailaddress, passw)
-    mail.select("inbox")
-    typ, data = mail.search(None, 'ALL')
-    unseen_messages = len(data[0].split())
-    print(unseen_messages)
-    if  unseen_messages > 0:
+    def __init__(self):
+        self.emailaddress = "blidohuset@gmail.com"
+        self.passw = "koppen123"
+        self.imapserver = "imap.gmail.com"
+    def delete_Unseen_Emails_and_get_user(self, user, password, IMAP):
+        mail = imaplib.IMAP4_SSL(IMAP)
+        mail.login(user, password)
+        mail.select("inbox")
+        typ, data = mail.search(None, 'ALL')
+        ids = data[0] # data is a list.
+        id_list = ids.split() # ids is a space separated string
+        latest_email_id = id_list[-1] # get the latest
+ 
+        result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
+        raw_email = data[0][1].decode('utf-8') 
+        email_message = email.message_from_string(raw_email)
+        sender = email.utils.parseaddr(email_message['From'])
+        mail.store(latest_email_id, '+FLAGS', r'(\Deleted)')
         mail.expunge()
         mail.close()
         mail.logout()
-        delete_Unseen_Emails_and_get_user(emailaddress, passw, imapserver)
-print('Trying')
-check_messages(emailaddress,passw)
+        return sender[1]
+
+    def check_messages(self, emailaddress,passw):
+        mail = imaplib.IMAP4_SSL(self.imapserver)
+        mail.login(emailaddress, passw)
+        mail.select("inbox")
+        sender = ''
+        typ, data = mail.search(None, 'ALL')
+        unseen_messages = len(data[0].split())
+        if  unseen_messages > 0:
+            print('Getting user')
+            mail.expunge()
+            mail.close()
+            mail.logout()
+            sender = self.delete_Unseen_Emails_and_get_user(self.emailaddress, self.passw, self.imapserver)
+            return sender
+        else:
+            return 0
