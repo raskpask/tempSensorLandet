3ba5b1f7bd6b0be94be92c97da1c48de90bc48bc
jakmol@ug.kth.se <MmR-PZ-Q>
2019-08-29 18:12:33 +0200

    New file for warning added

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -24,10 +24,10 @@ def get_humid():
     return round(humidity,1)
 
 def get_command(command):
-    if "off" or "Off" in command:
+    if Off" in command:
         warning_handler.warning_off()
         print("Warning is now turned off")
-    elif "on" or "On" in command:
+    elif "On" in command:
         warning_handler.warning_on()
         print("Warning is now turned on")
 def check_new_mails():
