0a6e05f72f4791cb9c82b695cad7ad16d3c764df
jakmol@ug.kth.se <MmR-PZ-Q>
2019-08-29 17:35:02 +0200

    New file for warning added

--- examples/mail.py
+++ examples/mail.py
@@ -23,12 +23,12 @@ class Mail_Handler:
         sender = email.utils.parseaddr(email_message['From'])
         subject = email_message['Subject']
         body = email_message.get_payload()
-        print(body[0])
+        print(body[1])
         mail.store(latest_email_id, '+FLAGS', r'(\Deleted)')
         mail.expunge()
         mail.close()
         mail.logout()
-        return sender[1],subject,body[0]
+        return sender[1],subject,body[1]
 
     def check_messages(self, emailaddress,passw):
         mail = imaplib.IMAP4_SSL(self.imapserver)
