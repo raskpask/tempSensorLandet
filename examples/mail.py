cfb7188e1e37395c8191547f05ce05fd6c2d0a81
jakmol@ug.kth.se <MmR-PZ-Q>
2019-09-02 20:15:29 +0200

    Some error handling added

--- examples/mail.py
+++ examples/mail.py
@@ -1,5 +1,6 @@
 import imaplib, smtplib
 import email
+from requests.exceptions import ConnectionError
 
 class Mail_Handler:
     
@@ -9,39 +10,45 @@ class Mail_Handler:
         self.imapserver = "imap.gmail.com"
 
     def delete_Unseen_Emails_and_get_user(self, user, password, IMAP):
-        mail = imaplib.IMAP4_SSL(IMAP)
-        mail.login(user, password)
-        mail.select("inbox")
-        _, data = mail.search(None, 'ALL')
-        ids = data[0] # data is a list.
-        id_list = ids.split() # ids is a space separated string
-        latest_email_id = id_list[-1] # get the latest
+        try:
+            mail = imaplib.IMAP4_SSL(IMAP)
+            mail.login(user, password)
+            mail.select("inbox")
+            _, data = mail.search(None, 'ALL')
+            ids = data[0] # data is a list.
+            id_list = ids.split() # ids is a space separated string
+            latest_email_id = id_list[-1] # get the latest
  
-        _, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
-        raw_email = data[0][1].decode('utf-8') 
-        email_message = email.message_from_string(raw_email)
-        sender = email.utils.parseaddr(email_message['From'])
-        subject = email_message['Subject']
-        mail.store(latest_email_id, '+FLAGS', r'(\Deleted)')
-        mail.expunge()
-        mail.close()
-        mail.logout()
-        return sender[1],subject
-
-    def check_messages(self, emailaddress,passw):
-        mail = imaplib.IMAP4_SSL(self.imapserver)
-        mail.login(emailaddress, passw)
-        mail.select("inbox")
-        _, data = mail.search(None, 'ALL')
-        unseen_messages = len(data[0].split())
-        if  unseen_messages > 0:
+            _, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
+            raw_email = data[0][1].decode('utf-8') 
+            email_message = email.message_from_string(raw_email)
+            sender = email.utils.parseaddr(email_message['From'])
+            subject = email_message['Subject']
+            mail.store(latest_email_id, '+FLAGS', r'(\Deleted)')
             mail.expunge()
             mail.close()
             mail.logout()
-            return self.delete_Unseen_Emails_and_get_user(self.emailaddress, self.passw, self.imapserver)
-        else:
-            return 0 , "Error no mail found"
-    
+        except ConnectionError as e:
+            print(e)    
+            return sender[1],subject
+
+    def check_messages(self, emailaddress,passw):
+        try:
+            mail = imaplib.IMAP4_SSL(self.imapserver)
+            mail.login(emailaddress, passw)
+            mail.select("inbox")
+            _, data = mail.search(None, 'ALL')
+            unseen_messages = len(data[0].split())
+            if  unseen_messages > 0:
+                mail.expunge()
+                mail.close()
+                mail.logout()
+                return self.delete_Unseen_Emails_and_get_user(self.emailaddress, self.passw, self.imapserver)
+            else:
+                return 0 , "Error no mail found"
+        except ConnectionError as e:
+            print(e)
+
     def send_message(self,recipient,subject,text):
         FROM = self.emailaddress
         TO = recipient if isinstance(recipient, list) else [recipient]
