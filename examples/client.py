03ebba1ac1d1c41bac3667bf959d9b40dded3184
jakmol@ug.kth.se <MmR-PZ-Q>
2020-05-30 23:02:23 +0200

    not static temp

--- examples/client.py
+++ examples/client.py
@@ -12,18 +12,18 @@ class Main:
         self.run()
 
     def getTemp(self):
-        return '4'
-        # _, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
-        # if type(temperature) == float:
-        #     return round(temperature, 1)
-        # return "Sensor error"
+        # return '25'
+        _, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
+        if type(temperature) == float:
+            return round(temperature, 1)
+        return "Sensor error"
 
     def getHumidity(self):
-        return '55'
-        # humidity, _ = Adafruit_DHT.read_retry(SENSOR, PIN)
-        # if type(humidity) == float:
-        #     return round(humidity, 1)
-        # return "Sensor error"
+        # return '60'
+        humidity, _ = Adafruit_DHT.read_retry(SENSOR, PIN)
+        if type(humidity) == float:
+            return round(humidity, 1)
+        return "Sensor error"
 
     def run(self):
         print("The program is running sending temp updates every 30 mins...")
