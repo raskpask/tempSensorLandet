d4abee42489857468c94ade12f31af934b4f8908
Jakob Molin <jakob.molin@konsult.folksam.se>
2020-09-21 23:56:08 +0200

    fronend added

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Chart from './Chart'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Temperatur på landet</h1>
+        <Chart/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Chart.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { LineChart } from 'react-chartkick'
+import 'chart.js'
+import * as tempData from './data/tempData.json'
+
+function MyChart() {
+  let temperatureData = '{}'
+  let humidityData = "{}"
+  let dataArray = tempData.data
+  console.log(dataArray)
+  for (let i =0; i < dataArray.length; i++){
+    temperatureData = temperatureData.split("}")[0] + '"' + dataArray[i].datetime + '": ' + dataArray[i].temp + ",}"
+    humidityData = humidityData.split("}")[0] + dataArray[i].datetime + ":" + dataArray[i].humid + ",}"
+  }
+  console.log(JSON.stringify(temperatureData))
+  
+let data = [
+  {
+    "name":"Temperatur (C°)", "data": {temperatureData}
+  },
+  {
+    "name":"Luftfuktigehet (%)","data": {"2017-05-13": 60, "2017-06-14": 5, "2018-07-14": 5, "2019-05-14": 15, "2019-05-30": 5, "2020-05-14": 25,}
+  }
+]
+console.log(data)
+return ( 
+  <div>
+    <LineChart min={0} max={100} width="1024px" height="800px" data={data} />
+  </div>
+);
+}
+
+export default MyChart
